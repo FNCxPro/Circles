3facdbfd3d079815cb09465223e4bb31cbdaa56f
Seth Stephens <relative@airmail.cc>
2018-04-06 13:40:18 -0400

    PM2 deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'Circles',
+    script: 'index.js',
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'node',
+      host: 'winter.fury.fun',
+      ref: 'origin/master',
+      repo: 'git@github.com:FNCxPro/Circles.git',
+      path: '/home/node/circles',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
