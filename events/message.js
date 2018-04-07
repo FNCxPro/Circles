2c874d652b8139d44917f87d24296892c9bc3d8e
Seth Stephens <relative@airmail.cc>
2018-04-07 07:05:10 -0400

    Blacklist phuckers

--- events/message.js
+++ events/message.js
@@ -1,4 +1,6 @@
+const db = require('../db')
 module.exports = async (client, handler, msg) => {
-  // TODO: DB initializing for user & guild
+  const user = await db.getUser(msg.author)
+  if (!!user.blacklisted) return
   await handler.onMessage(msg, msg.author, msg.guild)
 }
